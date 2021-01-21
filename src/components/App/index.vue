539f7045f49bf9eeea6f7b0ff6fe32ed46adcf51
mohnatus <furrycat.web@gmail.com>
2021-01-21 20:05:48 +0300

    Initial commit

---
+++ src/components/App/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Vue App component
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const process = require('process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	mode: process.env.mode || 'production',
+	entry: {
+		main: path.resolve(__dirname, './src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				sideEffects: true,
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: [
+					process.env.NODE_ENV !== 'production'
+						? 'style-loader'
+						: MiniCssExtractPlugin.loader,
+					'css-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Irregular verbs',
+			template: path.resolve(__dirname, './src/index.html'),
+			filename: 'index.html',
+		}),
+		new CleanWebpackPlugin(),
+		new VueLoaderPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.resolve(__dirname, './dist'),
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+	},
+};
