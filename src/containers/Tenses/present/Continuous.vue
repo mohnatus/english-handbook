6bbc06a6fe908c83bdc77eb3f5600e0480673700
FurryCat <hyp-hyper.cat@yandex.ru>
2021-02-11 23:35:33 +0300

    present continuous

---
+++ src/containers/Tenses/present/Continuous.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tense-block">
+    <div class="tense-construction">
+      <div class="tense-verb">am/is/are + V<sub>ing</sub></div>
+    </div>
+
+    <ul class="tense-cases">
+      <li class="tense-case">
+        <div class="tense-case__name">Действие в момент говорения</div>
+        <div class="tense-case__examples">
+          Be quiet, I'm reading a book.
+        </div>
+      </li>
+      <li class="tense-case">
+        <div class="tense-case__name">Начавшийся и еще не закончившийся процесс + временные состояния/ситуации</div>
+        <div class="tense-case__examples">
+          My friends are building a house. Maybe they'll finish it next summer.
+        </div>
+      </li>
+      <li class="tense-case">
+        <div class="tense-case__name">Постоянный процесс (always, constantly), процесс изменений, происходящих в настоящее время (особенно с get, change, become, increase, rise, fall, grow, improve, start, ...)</div>
+        <div class="tense-case__examples">
+          The Earth is always moving.
+          <br>
+          Is your English getting better?
+        </div>
+      </li>
+      <li class="tense-case">
+        <div class="tense-case__name">Запланированные действия в будущем (с высокой уверенностью)</div>
+        <div class="tense-case__examples">
+          I'm going to the doctor tomorrow, I have an appointment.
+          <br>
+          They are getting married in June.
+        </div>
+      </li>
+      <li class="tense-case">
+        <div class="tense-case__name">Постоянные действия, вызывающие эмоциональную реакцию</div>
+        <div class="tense-case__examples">
+          I've lost my pen again. I'm always losing things.
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
