a15f42c9132349fd4daa8fc0d121352ebe00aaca
FurryCat <hyp-hyper.cat@yandex.ru>
2021-02-11 23:41:08 +0300

    Past Continuous

---
+++ src/containers/Tenses/past/Continuous.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="tense-block">
+      <div class="tense-construction">
+        <div class="tense-verb">was/were + V<sub>ing</sub></div>
+        <div class="tense-auxiliary">did</div>
+      </div>
+      <ul class="tense-cases">
+        <li class="tense-case">
+          <div class="tense-case__name">Действие в конкретный момент в прошлом</div>
+          <div class="tense-case__examples">
+            I was doing homework yesterday at 5 o'clock.
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="tense-block">
+      <div class="tense-phrases">
+        <div class="tense-phrase">
+          <h2>+ Past Simple</h2>
+          <div class="tense-phrase__text">Одно действие в прошлом (Past Continuous) прерывается другим (Past Simple)</div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+</template>
